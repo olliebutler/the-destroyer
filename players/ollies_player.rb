f2e25773e33cf5dc2cc67df862a946fbee17134f
Oliver Butler Jones <oliverbutlerjones@gmail.com>
2017-10-04 16:23:33 +0100

    added hunt and destroy mode

--- players/ollies_player.rb
+++ players/ollies_player.rb
@@ -2,6 +2,9 @@ class OlliesPlayer
   def initialize
     @starting_used = []
     @attempts = []
+    @coords_hit = []
+    @previous_hits = 0
+    @previous_ships_remaining = 5
   end
 
   def name
@@ -135,22 +138,72 @@ class OlliesPlayer
   #
   # end
 
+  # dice.each do |r|
+  #     counts[r] += 1
+  #   end
+
   def repeated_attempt(coordinate)
     @attempts.include?(coordinate)
   end
 
   def take_turn(state, ships_remaining)
-    cheq_arr = chequered_array
-    coordinate = cheq_arr.sample
-    puts "coordinate #{coordinate}"
 
-    while repeated_attempt(coordinate)
-      coordinate = cheq_arr.sample
-      puts "coordinate in loops #{coordinate}"
+    if count_hits > @previous_hits && @previous_ships_remaining == ships_remaining
+      @previous_ships_remaining = ships_remaining
+      @previous_hits = count_hits(state)
+      attempt = destroy_mode
+      @attempts << attempt
+      return attempt
+    else
+      @previous_ships_remaining = ships_remaining
+      @previous_hits = count_hits(state)
+      attempt = hunt_mode
+      @attempts << attempt
+      return attempt
     end
-
-    return coordinate
   end
 
+def count_hits(state)
+  counts = Hash.new 0
+  state.each do |s|
+    s.each do |r|
+      counts[r] += 1
+    end
+  end
+  return counts[:hit]
+end
 
+def hunt_mode
+  cheq_arr = chequered_array
+  coordinate = cheq_arr.sample
+
+  while repeated_attempt(coordinate)
+    coordinate = cheq_arr.sample
+  end
+  return coordinate
+end
+
+def destroy_mode
+  start = @attempts.last
+
+  if valid_coord(start)
+end
+
+def surrounding_coords(coord)
+  up = coord
+  up[1] += 1
+  down = coord
+  down[1] -= 1
+  right = coord
+  right[0] += 1
+  left = coord
+  left[0] -= 1
+  [up,right,down,left]
+end
+
+def valid_coord(coord)
+  if coord[0] < 10 && coord[1] < 10
+    return true
+  else
+    false
 end
